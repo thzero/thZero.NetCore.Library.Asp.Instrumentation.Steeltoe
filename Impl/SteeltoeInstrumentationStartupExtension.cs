e7116602595cfcdf44ad5cda55181992706b16fe
thZero.com <development@thzero.com>
2018-11-10 19:28:37 -0600

    Updates for startup extensions

--- Impl/SteeltoeInstrumentationStartupExtension.cs
+++ Impl/SteeltoeInstrumentationStartupExtension.cs
@@ -36,11 +36,19 @@ namespace thZero.AspNetCore
     public class SteeltoeInstrumentationStartupExtension : IStartupExtension
     {
         #region Public Methods
-        public virtual void ConfigureInitializePost(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
+        public virtual void ConfigurePost(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
         {
         }
 
-        public virtual void ConfigureInitializePre(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
+        public virtual void ConfigurePre(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
+        {
+        }
+
+        public virtual void ConfigureInitializePost(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
+        {
+        }
+
+        public virtual void ConfigureInitializePre(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
         {
         }
 
@@ -72,11 +80,19 @@ namespace thZero.AspNetCore
         {
         }
 
-        public virtual void ConfigureServicesMvcPost(IServiceCollection services, IConfigurationRoot configuration)
+        public virtual void ConfigureServicesPost(IServiceCollection services, IConfigurationRoot configuration)
         {
         }
 
-        public virtual void ConfigureServicesMvcPre(IServiceCollection services, IConfigurationRoot configuration)
+        public virtual void ConfigureServicesPre(IServiceCollection services, IConfigurationRoot configuration)
+        {
+        }
+
+        public virtual void ConfigureServicesInitializeMvcPost(IServiceCollection services, IConfigurationRoot configuration)
+        {
+        }
+
+        public virtual void ConfigureServicesInitializeMvcPre(IServiceCollection services, IConfigurationRoot configuration)
         {
             ConfigureInitializeHealthContributors(services, configuration);
 
