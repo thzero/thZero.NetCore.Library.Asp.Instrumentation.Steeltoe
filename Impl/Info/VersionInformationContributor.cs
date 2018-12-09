c5f9b5aaf79e567e840e99038483ce37c1d7c7f8
thZero.com <development@thzero.com>
2018-12-09 08:32:25 -0600

    Copyright updates...

--- Impl/Info/VersionInformationContributor.cs
+++ Impl/Info/VersionInformationContributor.cs
@@ -1,5 +1,5 @@
 ﻿/* ------------------------------------------------------------------------- *
-thZero.NetCore.Library.Asp
+thZero.NetCore.Library.Asp.Instrumentation.Steeltoe
 Copyright (C) 2016-2018 thZero.com
 
 <development [at] thzero [dot] com>
