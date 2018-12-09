8bb9ac412bf149be7d3a5d83fba44ae89d95aa59
thZero.com <development@thzero.com>
2018-12-09 08:16:10 -0600

    Nuget updates, update to netcore2.2

--- Impl/SteeltoeInstrumentationStartupExtension.cs
+++ Impl/SteeltoeInstrumentationStartupExtension.cs
@@ -21,6 +21,7 @@ using System;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -36,11 +37,11 @@ namespace thZero.AspNetCore
     public class SteeltoeInstrumentationStartupExtension : IStartupExtension
     {
         #region Public Methods
-        public virtual void ConfigurePost(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
+        public virtual void ConfigurePost(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
         {
         }
 
-        public virtual void ConfigurePre(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
+        public virtual void ConfigurePre(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
         {
         }
 
@@ -52,11 +53,11 @@ namespace thZero.AspNetCore
         {
         }
 
-        public virtual void ConfigureInitializeFinalPre(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
+        public virtual void ConfigureInitializeFinalPre(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
         {
         }
 
-        public virtual void ConfigureInitializeFinalPost(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
+        public virtual void ConfigureInitializeFinalPost(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
         {
         }
 
@@ -80,19 +81,43 @@ namespace thZero.AspNetCore
         {
         }
 
-        public virtual void ConfigureServicesPost(IServiceCollection services, IConfigurationRoot configuration)
+        public virtual void ConfigureServicesPost(IServiceCollection services, IConfiguration configuration)
         {
         }
 
-        public virtual void ConfigureServicesPre(IServiceCollection services, IConfigurationRoot configuration)
+        public virtual void ConfigureServicesPre(IServiceCollection services, IConfiguration configuration)
         {
         }
 
-        public virtual void ConfigureServicesInitializeMvcPost(IServiceCollection services, IConfigurationRoot configuration)
+        public virtual void ConfigureServicesInitializeMvcBuilderPost(IMvcBuilder builder)
         {
         }
 
-        public virtual void ConfigureServicesInitializeMvcPre(IServiceCollection services, IConfigurationRoot configuration)
+        public virtual void ConfigureServicesInitializeMvcBuilderPre(IMvcBuilder builder)
+        {
+        }
+
+        public virtual void ConfigureServicesInitializeMvcBuilderPost(IMvcCoreBuilder builder)
+        {
+        }
+
+        public virtual void ConfigureServicesInitializeMvcBuilderPre(IMvcCoreBuilder builder)
+        {
+        }
+
+        public virtual void ConfigureServicesInitializeMvcOptionsPost(MvcOptions options)
+        {
+        }
+
+        public virtual void ConfigureServicesInitializeMvcOptionsPre(MvcOptions options)
+        {
+        }
+
+        public virtual void ConfigureServicesInitializeMvcPost(IServiceCollection services, IConfiguration configuration)
+        {
+        }
+
+        public virtual void ConfigureServicesInitializeMvcPre(IServiceCollection services, IConfiguration configuration)
         {
             ConfigureInitializeHealthContributors(services, configuration);
 
@@ -119,15 +144,15 @@ namespace thZero.AspNetCore
         #endregion
 
         #region Protected Methods
-        protected virtual void ConfigureInitializeHealthContributors(IServiceCollection services, IConfigurationRoot configuration)
+        protected virtual void ConfigureInitializeHealthContributors(IServiceCollection services, IConfiguration configuration)
         {
         }
 
-        protected virtual void ConfigureInitializeInfoContributors(IServiceCollection services, IConfigurationRoot configuration)
+        protected virtual void ConfigureInitializeInfoContributors(IServiceCollection services, IConfiguration configuration)
         {
         }
 
-        protected virtual void ConfigureInitializeMappingsContributors(IServiceCollection services, IConfigurationRoot configuration)
+        protected virtual void ConfigureInitializeMappingsContributors(IServiceCollection services, IConfiguration configuration)
         {
         }
         #endregion
