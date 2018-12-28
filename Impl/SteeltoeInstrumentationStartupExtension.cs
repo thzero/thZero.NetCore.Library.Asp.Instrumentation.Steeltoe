fead9c8cf0152806fb65ec2d9d10ece5d53ce4ab
thZero.com <development@thzero.com>
2018-12-28 10:28:13 -0600

    Converted to BaseStartupExtension

--- Impl/SteeltoeInstrumentationStartupExtension.cs
+++ Impl/SteeltoeInstrumentationStartupExtension.cs
@@ -19,13 +19,8 @@ limitations under the License.
 
 using System;
 
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 using Steeltoe.Management.Endpoint.Health;
 using Steeltoe.Management.Endpoint.Info;
@@ -34,91 +29,13 @@ using Steeltoe.Management.Endpoint.Metrics;
 
 namespace thZero.AspNetCore
 {
-    public class SteeltoeInstrumentationStartupExtension : IStartupExtension
+    public class SteeltoeInstrumentationStartupExtension : BaseStartupExtension
     {
         #region Public Methods
-        public virtual void ConfigurePost(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
+        public override void ConfigureServicesInitializeMvcPre(IServiceCollection services, IConfiguration configuration)
         {
-        }
+            base.ConfigureServicesInitializeMvcPre(services, configuration);
 
-        public virtual void ConfigurePre(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
-        {
-        }
-
-        public virtual void ConfigureInitializePost(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
-        {
-        }
-
-        public virtual void ConfigureInitializePre(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
-        {
-        }
-
-        public virtual void ConfigureInitializeFinalPre(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
-        {
-        }
-
-        public virtual void ConfigureInitializeFinalPost(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
-        {
-        }
-
-        public virtual void ConfigureInitializeRoutesBuilderPost(IRouteBuilder routes)
-        {
-        }
-
-        public virtual void ConfigureInitializeRoutesBuilderPre(IRouteBuilder routes)
-        {
-        }
-
-        public virtual void ConfigureInitializeSsl(IApplicationBuilder app)
-        {
-        }
-
-        public virtual void ConfigureInitializeStaticPost(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
-        {
-        }
-
-        public virtual void ConfigureInitializeStaticPre(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
-        {
-        }
-
-        public virtual void ConfigureServicesPost(IServiceCollection services, IConfiguration configuration)
-        {
-        }
-
-        public virtual void ConfigureServicesPre(IServiceCollection services, IConfiguration configuration)
-        {
-        }
-
-        public virtual void ConfigureServicesInitializeMvcBuilderPost(IMvcBuilder builder)
-        {
-        }
-
-        public virtual void ConfigureServicesInitializeMvcBuilderPre(IMvcBuilder builder)
-        {
-        }
-
-        public virtual void ConfigureServicesInitializeMvcBuilderPost(IMvcCoreBuilder builder)
-        {
-        }
-
-        public virtual void ConfigureServicesInitializeMvcBuilderPre(IMvcCoreBuilder builder)
-        {
-        }
-
-        public virtual void ConfigureServicesInitializeMvcOptionsPost(MvcOptions options)
-        {
-        }
-
-        public virtual void ConfigureServicesInitializeMvcOptionsPre(MvcOptions options)
-        {
-        }
-
-        public virtual void ConfigureServicesInitializeMvcPost(IServiceCollection services, IConfiguration configuration)
-        {
-        }
-
-        public virtual void ConfigureServicesInitializeMvcPre(IServiceCollection services, IConfiguration configuration)
-        {
             ConfigureInitializeHealthContributors(services, configuration);
 
             services.AddHealthActuator(configuration);
